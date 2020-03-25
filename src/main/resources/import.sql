46414783985aafc71bb8b71b5d1d7ecd7a301e2e
rammaciel <robertoaugusto00@outlook.com>
2020-03-25 18:14:17 -0300

    Versao 1

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao) values (1, 'Domingo', 22.0, 31.9, 57, 'Chuvas.');
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao) values (2, 'Segunda', 21.7, 28.7, 65, 'Sol.');
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao) values (3, 'Terça', 24.1, 29.0, 64, 'Sol e aumento de nuvens de manhã.');
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao) values (4, 'Terça', 12.1, 21.0, 84, 'Chuvas e garoas.');
